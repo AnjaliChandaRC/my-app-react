fbd2b5f8d9517a3a39fd7b6f05bda2af175e7111
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2022-09-28 10:22:15 +0530

    ist commit

---
+++ src/components/SubmitBtn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SubmitBtn = () => {
+  return (
+    <>sdsadjhaskjdaksdaskdjasdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</>
+  );
+};
+
+export default SubmitBtn;
