4dccf774f6aae15d7d8e7ad4e3cb09af2c216fa6
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2022-10-10 17:28:45 +0530

    some chnges

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { withRouter } from "react-router-dom";
 
 const Btn = ({ history }) => {
+  // const signupValue = localStorage.getItem("value");
+
+  // useEffect(() => {
+  //   console.log("signupValu;;;;;;;;;;;e", signupValue);
+  //   if (signupValue === "true") {
+  //     history.push("/");
+  //   }
+  // }, [signupValue]);
   const signinForm = (e) => {
     history.push("/sign-in");
+    // localStorage.setItem("value", "true");
+    // // console.log("initialValue", initialValue);
+    // history.push("/");
   };
 
   const loginForm = (e) => {
     history.push("/login");
   };
+
   return (
     <>
       <div className="container flex-column d-flex align-items-center">
